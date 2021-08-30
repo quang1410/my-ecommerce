eb0fc7dd520a80955db6612ab1d80592d5e62602
unknown <quachtuananh2016@gmail.com>
2021-08-30 22:54:12 +0700

    code product-detail

--- packages/web-client/src/pages/user/Product.jsx
+++ packages/web-client/src/pages/user/Product.jsx
@@ -1,13 +1,57 @@
 import React from 'react'
 
-import Partial from '../../components/user/Partial'
 import Helmet from '../../components/user/Helmet'
+import Partial from '../../components/user/Partial'
+import Section, {SectionBody, SectionTitle} from '../../components/user/Section'
+import Grid from '../../components/user/Grid'
+import ProductCard from '../../components/user/ProductCard'
+import ProductView from '../../components/user/ProductView'
 
-const Product = (props) => {
+import productData from '../../assets/fake-data/products'
+
+const Product = props => {
+    const product = productData.getProductBySlug(props.match.params.slug)
+
+    const relatedProducts = productData.getProducts(8)
+
+    React.useEffect(() => {
+        window.scrollTo(0,0)
+    }, [product])
+
     return (
         <Partial>
-            <Helmet title="Trang chi tiết">
-                Product detail
+            <Helmet title={product.title}>
+                <Section>
+                    <SectionBody>
+                        <ProductView product={product}/>
+                    </SectionBody>
+                </Section>
+                <Section>
+                    <SectionTitle>
+                        Khám phá thêm
+                    </SectionTitle>
+                    <SectionBody>
+                        <Grid
+                            col={4}
+                            mdCol={2}
+                            smCol={1}
+                            gap={20}
+                        >
+                            {
+                                relatedProducts.map((item, index) => (
+                                    <ProductCard
+                                        key={index}
+                                        img01={item.image01}
+                                        img02={item.image02}
+                                        name={item.title}
+                                        price={Number(item.price)}
+                                        slug={item.slug}
+                                    />   
+                                ))
+                            }
+                        </Grid>
+                    </SectionBody>
+                </Section>
             </Helmet>
         </Partial>
     )
