45ca94ab5290a6c8f2a4d6532848950b4c0ef50a
unknown <quachtuananh2016@gmail.com>
2021-09-01 14:33:59 +0700

    fix css login

--- packages/web-client/src/components/user/Form/Login.jsx
+++ packages/web-client/src/components/user/Form/Login.jsx
@@ -70,77 +70,79 @@ const Login = () => {
         login()
     },[])
     return (
-      <div class="form" id="signin-form">
-        <a href="/" class="form-logo">
-          <img src={logo} alt="company logo" />
-        </a>
-        <div class="form-group">
-          <input
-            type="email"
-            class="form-input"
-            placeholder="Email"
-            id="signin-email"
-          />
-          <span class="form-input-icon err">
-            <i class="bx bx-error-circle"></i>
-          </span>
-          <span class="form-input-icon success">
-            <i class="bx bx-check-circle"></i>
-          </span>
-          <span class="form-input-err-msg" data-err-for="signin-email"></span>
-        </div>
-        <div class="form-group">
-          <input
-            type="password"
-            class="form-input"
-            placeholder="Password"
-            id="signin-password"
-          />
-          <span class="form-input-icon err">
-            <i class="bx bx-error-circle"></i>
-          </span>
-          <span class="form-input-icon success">
-            <i class="bx bx-check-circle"></i>
-          </span>
-          <span
-            class="form-input-err-msg"
-            data-err-for="signin-password"
-          ></span>
-        </div>
-        <div class="form-group form-group-checkbox">
-          <input
-            type="checkbox"
-            name="remember"
-            id="remember"
-            checked="checked"
-          />
-          <label for="remember">
-            Remember Me
-            <i class="bx bx-check"></i>
-          </label>
-        </div>
-        <button class="form-btn" id="signin-btn">
-          Sign in
-        </button>
-        <span class="form-delimiter">or connect with</span>
-        <div class="form-social">
-          <a href="/" class="form-social-item fb">
-            <i class="bx bxl-facebook"></i>
+      <div className="container-login">
+        <div class="form" id="signin-form">
+          <a href="/" class="form-logo">
+            <img src={logo} alt="company logo" />
           </a>
-          <a href="/" class="form-social-item tw">
-            <i class="bx bxl-twitter"></i>
-          </a>
-          <a href="/" class="form-social-item gg">
-            <i class="bx bxl-google"></i>
-          </a>
+          <div class="form-group">
+            <input
+              type="email"
+              class="form-input"
+              placeholder="Email"
+              id="signin-email"
+            />
+            <span class="form-input-icon err">
+              <i class="bx bx-error-circle"></i>
+            </span>
+            <span class="form-input-icon success">
+              <i class="bx bx-check-circle"></i>
+            </span>
+            <span class="form-input-err-msg" data-err-for="signin-email"></span>
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              class="form-input"
+              placeholder="Password"
+              id="signin-password"
+            />
+            <span class="form-input-icon err">
+              <i class="bx bx-error-circle"></i>
+            </span>
+            <span class="form-input-icon success">
+              <i class="bx bx-check-circle"></i>
+            </span>
+            <span
+              class="form-input-err-msg"
+              data-err-for="signin-password"
+            ></span>
+          </div>
+          <div class="form-group form-group-checkbox">
+            <input
+              type="checkbox"
+              name="remember"
+              id="remember"
+              checked="checked"
+            />
+            <label for="remember">
+              Remember Me
+              <i class="bx bx-check"></i>
+            </label>
+          </div>
+          <button class="form-btn" id="signin-btn">
+            Sign in
+          </button>
+          <span class="form-delimiter">or connect with</span>
+          <div class="form-social">
+            <a href="/" class="form-social-item fb">
+              <i class="bx bxl-facebook"></i>
+            </a>
+            <a href="/" class="form-social-item tw">
+              <i class="bx bxl-twitter"></i>
+            </a>
+            <a href="/" class="form-social-item gg">
+              <i class="bx bxl-google"></i>
+            </a>
+          </div>
+          <span class="form-txt">
+            Don't have an account?
+            <a href="/"> Sign up!</a>
+          </span>
+          <span class="form-txt">
+            <a href="/">Forgot password?</a>
+          </span>
         </div>
-        <span class="form-txt">
-          Don't have an account?
-          <a href="/"> Sign up!</a>
-        </span>
-        <span class="form-txt">
-          <a href="/">Forgot password?</a>
-        </span>
       </div>
     );
 }
